06927330be6869c92a5b7bee45f2f37d22dee688
Arsen <arsenko525@gmail.com>
2021-05-25 20:02:04 +0300

    udate

--- 12_3/12_3/12_3.cpp
+++ 12_3/12_3/12_3.cpp
@@ -1,74 +1,83 @@
 ﻿#include <iostream>
 #include <Windows.h>
-#include <time.h>
-
-typedef int Info;
-struct Elem
-{
-    Elem* next,
-        * prev;
-    Info info;
-};
 
 using namespace std;
+
+typedef int Info; 
+
+struct Elem
+{
+	Elem* next;
+	Elem* prev;
+	Info info;
+};
+
 void enqueue(Elem*& first, Elem*& last, Info value);
-void Check(Elem* L, Info x);
-void Print(Elem* L);
+void print(Elem* L);
+void process(Elem* L, Info value);
 
 int main()
 {
-    srand((unsigned)time(NULL));
-    SetConsoleCP(1251); // встановлення сторінки win-cp1251 в потік вводу
-    SetConsoleOutputCP(1251); // встановлення сторінки win-cp1251 в потік виводу
 
-    Elem* first = NULL,
-        * last = NULL;
-    for (int i = 1; i <= 10; i++) {
-        Info numb = 1 + rand() % 10;
-        enqueue(first, last, numb);
-    }
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251); // забезпечення відображення кирилиці в консолі
 
-    cout << endl;
-    Print(first);
-    cout << endl;
-    int x; 
-    cout << "Введіть число, яке буде провірятись на дублювання: "; cin >> x;
-    cout << endl;
-    Check(first, x);
-    cout << endl;
+	Elem* first = NULL,
+		* last = NULL; 
+
+	Info num;
+	for (int i = 1; i <= 10; i++) {
+		num = 1 + rand() % 10; 
+		enqueue(first, last, num); 
+	}
+	cout << "Оригінальний список: " << endl << endl;
+	print(first); // вивід черги
+	cout << endl;
+
+	Info info;
+	cout << "Введіть значення інформаційного поля, яке буде продубльовано в списку: "; cin >> info;
+	cout << endl;
+	process(first, info); 
+
+	print(first);
+
+	return 0;
 }
-
-
 void enqueue(Elem*& first, Elem*& last, Info value)
 {
-    Elem* tmp = new Elem;
-    tmp->info = value;
-    tmp->next = NULL;
-    if (last != NULL)
-        last->next = tmp;
-    tmp->prev = last;
-    last = tmp;
-    if (first == NULL)
-        first = tmp;
+	Elem* tmp = new Elem;
+	tmp->info = value;
+	tmp->next = NULL;
+	if (last != NULL)
+		last->next = tmp;
+	tmp->prev = last;
+	last = tmp;
+	if (first == NULL)
+		first = tmp;
 }
 
-void Print(Elem* L) {
-    cout << "Вивід списку: ";
-    while (L->next != NULL)
-    {
-        cout << L->info << " ";
-        L = L->next;
-    }
+void print(Elem* L) // функція виведення списку
+{
+	while (L != NULL) 
+	{
+		cout << L->info << " "; 
+		L = L->next; 
+	}
+	cout << endl;
 }
 
-void Check(Elem* L, Info x) {
-    cout << "Дублювання однакових цифр: ";
-    while (L->next != NULL)
-    {
-        cout << L->info << " ";
-        if (L->info == x) { // Якщо число знайдено, то воно дублюється
-            cout << "*" << L->info << "*" << " ";
-        }
-        L = L->next;
-    }
+void process(Elem* L, Info value) // функція опрацювання списку
+{
+	while (L != NULL) 
+	{
+		if (L->info == value) 
+		{
+			Elem* tmp = new Elem;  
+			tmp->info = value;  
+			tmp->next = L->next; 
+			L->next = tmp; 
+			L = L->next; 
+		}
+		L = L->next; 
+	}
 }
